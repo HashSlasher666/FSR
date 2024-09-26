488cb7a0488e043fb59eb40db03a5a06fbda97ff
HashSlasher666 <126615883+HashSlasher666@users.noreply.github.com>
2024-09-26 17:53:18 +0300

    parser.cpp

---
+++ TSP3OPT/parser.cpp
@@ -0,0 +1,25 @@
+#include "parser.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+Parser::Parser(const std::string& filename) : filename(filename) {}
+
+std::vector<Point> Parser::parseData() {
+    std::vector<Point> points;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Unable to open file " << filename << std::endl;
+        return points;
+    }
+    
+    int N;
+    file >> N;
+    for (int i = 0; i < N; ++i) {
+        Point p;
+        file >> p.x >> p.y;
+        points.push_back(p);
+    }
+    
+    return points;
+}
