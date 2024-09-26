74f093fa2ed7eaa947b585d4b8d8163d6366e49e
HashSlasher666 <126615883+HashSlasher666@users.noreply.github.com>
2024-09-26 17:52:11 +0300

    main.cpp

---
+++ TSP3OPT/main.cpp
@@ -0,0 +1,30 @@
+#include "parser.h"
+#include "solver.h"
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string filename;
+    std::cout << "Enter the filename: ";
+    std::cin >> filename;
+
+    Parser parser(filename);
+
+    std::vector<Point> points = parser.parseData();
+
+    if (points.empty()) {
+        std::cerr << "Error: no points loaded from the file." << std::endl;
+        return 1;
+    }
+
+    Solver solver(points);
+    solver.solve();
+
+    solver.printSolution();
+
+    std::cout << "Press Enter to exit...";
+    std::cin.ignore();  // Игнорируем остатки символов в буфере
+    std::cin.get();
+
+    return 0;
+}
